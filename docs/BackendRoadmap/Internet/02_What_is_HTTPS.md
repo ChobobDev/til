325ab0939da0c5ab12831c2afb6c8716cc7742c3
chobobdev <chobobdev@naver.com>
2021-11-30 09:54:18 +0900

    [Update] HTTPS

--- docs/BackendRoadmap/Internet/02_What_is_HTTPS.md
+++ docs/BackendRoadmap/Internet/02_What_is_HTTPS.md
@@ -2,3 +2,7 @@
 title: What Is HTTPS
 sidebar_position: 2
 ---
+
+HTTPS를 알기전 HTTP를 먼저 알아가는 것이 좋을것 같다고 생각합니다.
+HTTP란 Hyper Text Transfer Potocol의 acronym 입니다.
+`Hyper Text Transfer Potocol` 를 풀어 해석을 해본다면 하이퍼 텍스트를 전송하기위한 규악으로 해석될 수 있다.
--- docusaurus.config.js
+++ docusaurus.config.js
@@ -81,7 +81,7 @@ const config = {
             ],
           },
         ],
-        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+        copyright: `Copyright © ${new Date().getFullYear()} TIL, Inc. Built with Docusaurus.`,
       },
       prism: {
         theme: lightCodeTheme,
